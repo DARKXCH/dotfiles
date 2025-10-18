c5b701446435151f8df7095b98c497504f84da61
NOEIS <darkxch41@163.com>
2025-10-18 23:13:28 +0800

    update fish config and remove discord config and add vesktop config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -4,4 +4,9 @@ end
 # environment variable
 set -gx EDITOR helix
 #alias
-alias hx="helix"
+#alias hx="helix"
+
+#abbr
+abbr --add ff fastfetch
+abbr --add hx helix
+abbr --add discord "discord --ozone-platform-hint=auto"
